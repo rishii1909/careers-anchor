97418e9134ca0e40895822fcdb634fbd3d314d23
Hrushikesh Chapke <hrushikeshchapke@gmail.com>
2022-01-11 03:56:23 +0530

    Finished and sent to ma'am for review

--- src/App.js
+++ src/App.js
@@ -2,19 +2,93 @@
 import React, { useState } from 'react';
 
 import './App.css';
-import { Button, Form, Slider, Divider, Progress } from 'antd';
-// import { FrownOutlined, SmileOutlined } from '@ant-design/icons';
+import { Button, Form, Slider, Divider, Progress, Row, Col, Table, message } from 'antd';
+import { ExperimentOutlined, DownloadOutlined } from '@ant-design/icons';
+
 import questions from './questions';
+import RadarChart from '@ant-design/plots/es/components/radar';
+import printHtmlToPDF from "print-html-to-pdf";
+
+var responses = [];
+
+// For testing
+// for (let i = 0; i < questions.length; i++) {
+//   responses[i] = Math.floor(Math.random() * 11)*10;
+  
+// }
+
+const anchors = {
+  0 : "Security, Stability, Organizational Identity",
+  1 : "Autonomy / Independence",
+  2 : "Technical / Functional Competence",
+  3 : "Managerial Competence",
+  4 : "Entrepreneurial Creativity",
+  5 : "Sense of Service / Dedication to a Cause",
+  6 : "Pure Challenge",
+  7 : "Life-Style Integration"
+}
 
 function App() {
-  let responses = {};
+  
   const [response_counter, setResponse_counter] = useState(0);
+  const [result, setResult] = useState([]);
+  const [filled, setFilled] = useState(false);
+  const [printing, setPrinting] = useState(false);
+  const [downloaded, setDownloaded] = useState(false);
+
 
   function onFinish(values){
-    console.log(values)
+
+    let condense = [];
+    for (let i = 0; i < questions.length; i++) {
+      const el = responses[i];
+      const addr = (i) % 8;
+      if(addr === 0) console.log(i, el)
+      if(condense[addr] === undefined){
+        condense[addr] = {
+          label : anchors[addr],
+          value : el
+        };
+      }
+      else condense[addr].value += el;
+    }
+    condense.sort((a,b) => {
+      if(a.value < b.value) return 1;
+      if(a.value > b.value) return -1;
+      return 0
+
+    })
+    console.log(condense);
+    setResult(condense);
+    setFilled(true);
+    
+    
   }
+
+  async function downloadPDF(){
+    setPrinting(true);
+    const node = document.getElementById('app-root');
+    const pdfOption = {
+      jsPDF: {
+        unit: 'px',
+        format: 'letter',
+      },
+       spin: false,
+       fileName: 'Career_Anchors_Assessment_Result',
+       // You can hide element which you don't want to be part of pdf
+       hideDomNodeUsingGivenSelectors: {
+         id: [],
+         class: ['download-button'],
+         nodes: []
+       },
+    }
+    await printHtmlToPDF.print(node, pdfOption);
+    setPrinting(false);
+    setDownloaded(true);
+   };
+
   return (
-    <div className="App">
+    <div className="App" id='app-root'> 
       <div className='responsive-container'>
         <h1 style={{textAlign : "left", borderBottom : '1px solid lightGray'}} > Motivation and Career Anchors Assessment</h1>
         <p>
@@ -32,37 +106,94 @@ function App() {
             scrollToFirstError
             colon={false}
             wrapperCol={{ span: 14 }}
-            // onFinish={onFinish}
-            // onFinishFailed={onFinishFailed} 
             autoComplete="off"
             onFinish={onFinish}
+            onFinishFailed={() => message.error("Please make a choice for each of the given statements.")}
+            disabled
           >
             {questions.map((q, i) =>
               <Form.Item
-              name={`question_${i}`}
+              // name={`question_${}`}
+              name={i}
+              key={i}
               label={`Q${i+1}. ${q.text}`}
               style={{paddingBottom : '1em', borderBottom : '1px solid lightGray'}}
               rules={[{required : true, message : "Please make a choice."}]}
+              // initialValue={responses[i]}
               >
                 <Slider
-                  defaultValue={50}  
+                defaultValue={50}
                   min={0}
                   step={10}
                   tipFormatter={formatter[q.type]}
+                  tooltipVisible={true}
                   max={100}
-                  onChange={(val) => {setResponse_counter(response_counter + 1); responses[i] = val}}
+                  onChange={(val) => {
+                    if(responses[i] === undefined) setResponse_counter(response_counter + 1);
+                    responses[i] = val;
+                    if(filled) setFilled(false);
+                  }}
+                  disabled={filled}
                 />
               </Form.Item>
             )}
-            <Button type='primary' htmlType='submit'>Submit</Button>
-            <div style={{padding : '0.4em', position : 'fixed', left : '1em', bottom : "1em", backgroundColor : 'white', borderRadius : '4px', boxShadow : '-1px 2px 10px 5px lightGray' , width: 170}} >
-            My progress
-              <Progress percent={Math.round(response_counter*100 / questions.length)}></Progress>
-              {response_counter >= questions.length && 
-                <Button type="primary" size='small' htmlType='submit' style={{marginTop : "0.4em"}}>Submit responses</Button>
+            {
+              !filled && 
+              <div style={{display : 'flex', alignItems : "flex-end", justifyContent : 'flex-end'}}>
+                <Button style={{borderRadius : '3px', boxShadow : '-0.5px 0.5px 4px 1px gray'}} icon={<ExperimentOutlined />} type='primary' htmlType='submit'>Analyze responses ! </Button>
+              </div>
+            }
+            <div className='progress-container' style={{padding : '0.6em', position : 'fixed', left : '1em', bottom : "1em", backgroundColor : 'white', borderRadius : '4px', boxShadow : '-1px 2px 10px 5px lightGray' , width: 170}} >
+            Your progress
+              <Progress 
+                percent={Math.round(response_counter*100 / questions.length)} 
+                // percent={100} 
+                strokeColor={{
+                  // from: '#FF674D',
+                  from: '#034748',
+                  to : '#1481BA'
+                }} 
+              />
+              {(response_counter === questions.length && !filled) && 
+                <Button type="primary" size='small' htmlType='submit' style={{marginTop : "0.4em", boxShadow : '-0.5px 0.5px 4px 1px gray'}} icon={<ExperimentOutlined />}>Analyze !</Button>
               }
               </div>
           </Form>
+          {result.length > 0 && 
+            <>
+            <Divider/>
+            <Row justify='space-between' align='middle' className='results-container'>
+              <Col md={24} lg={24} xl={10} style={{marginBottom : '1.4em'}} >
+              <h2 style={{textAlign : "left", borderBottom : '1px solid lightGray'}}  > Here are your results! 🎉</h2>
+              <Table 
+              dataSource={result}
+              pagination={false}
+              columns={[
+                {
+                  title : 'Points',
+                  dataIndex : 'value',
+                  key : 'value'
+                },
+                {
+                  title : "Career Anchor",
+                  dataIndex : 'label',
+                  key : 'label'
+                }
+              ]}></Table>
+              </Col>
+              <Col md={24} lg={24} xl={14} style><RadarChart style={{marginBottom : '1.4em'}} data={result} {...config}/></Col>
+            </Row>
+            <div style={{padding : '1.2em', }} >
+              <p>The highest score is your Career Anchor.  If any two scores are close (within 10 points), read the description of each of the two Career Anchors and see which one you feel best describes your true preference.  The Career Anchor indicated by your second highest score may also be an important indicator and should also be recorded.  The Career Anchor descriptions follow the scoring form.  If one of the descriptions seems to be accurate with your own self-perceptions but is not your highest number, use that one as your Career Anchor, regardless of the scores. </p>
+                <div style={{justifyContent : 'right', display : 'flex', paddingTop : '1em'}}>
+                  <Button loading={printing} onClick={()=>downloadPDF()} type="primary" className={downloaded ? 'download-button' : 'download-button wiggle'} shape="round" icon={<DownloadOutlined /> }>
+                    Download Assessment Result
+                  </Button>
+                </div>
+            </div>
+            
+            </>
+          }
         </div>
       </div>
       
@@ -83,5 +214,40 @@ const formatter = {
     return `I agree ${val}%.`
   }
 } 
+
+const config = {
+  xField: 'label',
+  yField: 'value',
+  meta: {
+    value: {
+      alias: 'Points',
+      min: 0,
+      nice: true,
+      // formatter: (v) => Number(v).toFixed(2),
+    },
+  },
+  appendPadding: [0, 10, 0, 10],
+  color : '#1481BA',
+  // meta: {
+  //   value: {
+  //     alias: 'Points',
+  //     min: 0,
+  //     max: 80,
+  //   },
+  // },
+  xAxis: {
+    tickLine: null,
+  },
+  yAxis: {
+    label: false,
+    grid: {
+      alternateColor: 'rgba(0, 0, 0, 0.04)',
+    },
+  },
+  area: {},
+  point: {
+    size: 2,
+  },
+};
 
 export default App;
--- src/questions.js
+++ src/questions.js
@@ -80,44 +80,180 @@ var questions = [
         type : '0'
     },
     {
-        text : "",
+        text : "To start from scratch and create something new and original is",
         type : '0'
     },
     {
-        text : "",
+        text : "Being of service to others in a meaningful way is",
         type : '0'
     },
     {
-        text : "",
+        text : "Reaching and growing beyond my current level is",
         type : '0'
     },
     {
-        text : "",
+        text : "Flexibility in location, work, and hours",
         type : '0'
     },
     {
-        text : "",
+        text : "Knowing where I will be working and what I will be doing year after year is",
         type : '0'
     },
     {
-        text : "",
+        text : "The freedom to choose the direction of my career path is",
         type : '0'
     },
     {
-        text : "",
+        text : "The ability to use technical skills or knowledge to complete a clearly defined project is",
         type : '0'
     },
     {
-        text : "",
+        text : "Supervising, leading, and influencing others is",
         type : '0'
     },
     {
-        text : "",
+        text : "Having the challenge of creating something new is",
         type : '0'
     },
     {
-        text : "",
+        text : "Knowing that my work is contributing to the well-being of others is",
         type : '0'
+    },
+    {
+        text : "Having problems that stretch my abilities is",
+        type : '0'
+    },
+    {
+        text : "Having sabbaticals or other leaves to pursue activities outside of work is",
+        type : '0'
+    },
+    {
+        text : "I would be more interested in remaining in my present location rather than moving even if it offered a promotion",
+        type : '1'
+    },
+    {
+        text : "I am more concerned with my own freedom and autonomy than with any other factor in my work life",
+        type : '1'
+    },
+    {
+        text : "I am very proud of my technical and functional competence",
+        type : '1'
+    },
+    {
+        text : "Managing other people gives me a strong sense of achievement",
+        type : '1'
+    },
+    {
+        text : "Owning my own business is very important to me",
+        type : '1'
+    },
+    {
+        text : "I am turned on by being dedicated to a worthwhile cause",
+        type : '1'
+    },
+    {
+        text : "Projects are interesting to me only when they are really challenging",
+        type : '1'
+    },
+    {
+        text : "I am more interested in an integrated lifestyle than in career promotion",
+        type : '1'
+    },
+    {
+        text : "I need to belong to an organization to feel satisfied in my work life",
+        type : '1'
+    },
+    {
+        text : "The more freedom I have to do what I want the happier I am at work",
+        type : '1'
+    },
+    {
+        text : "I would be interested in being a manager only if I could continue working in my area of expertise",
+        type : '1'
+    },
+    {
+        text : "It gives me great satisfaction to be promoted into positions where I can exercise greater management authority",
+        type : '1'
+    },
+    {
+        text : "Creating new business ideas and turning them into reality has been something I’ve wanted to do for a long time",
+        type : '1'
+    },
+    {
+        text : "I enjoy volunteering with dedication to a cause even when there is little or no compensation",
+        type : '1'
+    },
+    {
+        text : "I would live or work almost anywhere if the job had some real challenges",
+        type : '1'
+    },
+    {
+        text : "My family, my hobbies, my friends, and my recreation are just as important to me as my work",
+        type : '1'
+    },
+    {
+        text : "I would be unhappy working without job security",
+        type : '1'
+    },
+    {
+        text : "I have difficulty dealing with organizational restraints.",
+        type : '1'
+    },
+    {
+        text : "Becoming more knowledgeable in my field is a great source of pride and satisfaction",
+        type : '1'
+    },
+    {
+        text : "Supervising others and leading then to the achievement of targets and results is very important to me",
+        type : '1'
+    },
+    {
+        text : "The thrill and tension of being on the edge in my own business is a strong motivator for me",
+        type : '1'
+    },
+    {
+        text : "I think I have a contribution to make toward improving the world and helping people",
+        type : '1'
+    },
+    {
+        text : "The challenge of competition turns me on to produce my best work",
+        type : '1'
+    },
+    {
+        text : "I am only satisfied at work when my family life gets adequate attention",
+        type : '1'
+    },
+    {
+        text : "I like being part of a large organization where I can be certain that my job, work, and salary are relatively secure",
+        type : '1'
+    },
+    {
+        text : "I like working in a position that allows freedom and latitude",
+        type : '1'
+    },
+    {
+        text : "I feel best when I can solve technical problems in my area of expertise",
+        type : '1'
+    },
+    {
+        text : "I feel best when others come to me for advice about how to get their jobs done most effectively",
+        type : '1'
+    },
+    {
+        text : "I want own my own ideas, turn them into reality, and benefit from the money earned by creating something new",
+        type : '1'
+    },
+    {
+        text : "I care more about people, the environment, peace, and other critical issues than I do about promotions and success",
+        type : '1'
+    },
+    {
+        text : "No pain, no gain!",
+        type : '1'
+    },
+    {
+        text : "Happiness is a function of a balanced and integrated lifestyle",
+        type : '1'
     },
 ]
 
